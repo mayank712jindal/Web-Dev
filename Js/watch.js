cf857e77aa81e6e8ea421d04ad53af8423f9ba20
Mayank Jindal <mayank712jindal@gmail.com>
2021-03-15 22:43:43 +0530

    watch completed

--- Js/watch.js
+++ Js/watch.js
@@ -4,15 +4,28 @@ $(document).ready(function(){
         $("img").attr("src",link);
     });
 
-    clockUpdate();
-    setInterval(clockUpdate,1000);
+     clockUpdate();
+    var timer= setInterval(clockUpdate,1000);
 
     function clockUpdate(){
         var time=new Date();
         var h=time.getHours();
         var m=time.getMinutes();
         var s=time.getSeconds();
-
-    
+        $(".watch-face").text(h+":"+m+":"+s);
     }
+    $(".time").click(function(){
+        clockUpdate();
+        var timer= setInterval(clockUpdate,1000);
+    });
+    $(".heartRate").click(function(){
+        $(".watch-face").text("78");
+        clearInterval(timer);
+    });
+    // 
+    var height=$("img").height();
+    var width=$("img").width();
+
+    $(".watch").css("height",height);
+    $(".watch").css("width",width);
 });
