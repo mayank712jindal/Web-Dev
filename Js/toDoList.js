327deff76ea69a3f1d2c898239f24c5b6029c2c4
Mayank Jindal <mayank712jindal@gmail.com>
2021-04-01 22:56:01 +0530

    TodoList completed

--- Js/toDoList.js
+++ Js/toDoList.js
@@ -1,8 +1,20 @@
-function Add(){
-    var text= document.getElementById("listText").value;
-    console.log(text)
-    var node=document.createElement("li");
-    var textnode= document.createTextNode(text);
-    node.appendChild(textnode);
-    document.getElementById("list").appendChild(node);
-}
+$("#add-item").click(()=>{
+    var text= $("#listText").val();
+    if(text.length===0)
+    alert("Please enter some data!");
+    else{
+        $("#list-items").append("<p>"+text+"<span><img src=\"https://img.icons8.com/ios/50/000000/delete-forever--v1.png\"  class=\"delete\"/></span></p>");
+    }
+});
+
+$("#remove-item").click(()=>{
+        $("#list-items").empty();
+});
+
+$("#list-items").on("click",".delete",function(){
+    $(this).parentsUntil("#list-items").remove();
+});
+
+$("#list-items").on("click","p",function(){
+    $(this).toggleClass("marked");
+})
