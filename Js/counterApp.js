8b48d4aed3ca4effe27d0a01a7c89b4279928340
Mayank Jindal <mayank712jindal@gmail.com>
2021-04-01 10:03:32 +0530

    counterApp completed

--- Js/counterApp.js
+++ Js/counterApp.js
@@ -1,18 +1,42 @@
 var value=$("#count").text();
-// if(value>10)
-// $("#add").attr("disabled", true);
+function view(value){
+    if(value%2==0)
+    {
+     $("body").css("background-color","skyblue");
+     $("#count").css("color","white");
+        }
+    else
+    {
+     $("body").css("background-color","grey");
+     $("#count").css("color","red");
+        }
 
-console.log(value)
-
+        if(value==10)
+        $("#add").prop("disabled", true);
+        else
+        $("#add").prop("disabled", false);
+        if(value==0)
+        $("#minus").prop("disabled", true);
+        else
+        $("#minus").prop("disabled", false);
+}
 $("#add").click(()=>{
     value++;
+    view(value);
+
+    if(value>=10)
+    $("#add").prop("disabled", true);
+    else
+    $("#add").prop("disabled", false);
     $("#count").text(value);
 });
 $("#minus").click(()=>{
     value--;
+    view(value);
     $("#count").text(value);
 });
 $("#reset").click(()=>{
     value=0;
+    view(value);
     $("#count").text(value);
 });
